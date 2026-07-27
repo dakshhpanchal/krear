07a7ad74ba112b8fbf0a5c754984993feab971ce
dakshhpanchal <dakshpanchal08@gmail.com>
2026-07-28 01:31:01 +0530

    changes to entrypoint

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 python manage.py collectstatic --noinput
 python manage.py migrate --noinput
-celery -A config worker -l info --concurrency=2 &
-exec gunicorn config.wsgi:application --bind 0.0.0.0:8000 --workers 2 --timeout 120
+celery -A config worker -l info --concurrency=1 --pool=solo &
+exec gunicorn config.wsgi:application --bind 0.0.0.0:8000 --workers 1 --timeout 120
